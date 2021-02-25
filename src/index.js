998c3121fdaa573678ef1a60d8b7e663f7c2aaf6
Abhishek Chakravarty <abhishek.chakravarthy@ellucian.com>
2021-02-26 01:32:48 +0530

    Integrated Speechly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import { Provider } from './context/context';
+import { SpeechProvider } from '@speechly/react-client';
 import App from './App';
 import './index.css';
 
 ReactDom.render(
-  <Provider>
-    <App />
-  </Provider>,
+  <SpeechProvider appId="538e999c-8395-426d-ae83-68035a377d56" language="en-US">
+    <Provider>
+      <App />
+    </Provider>
+  </SpeechProvider>,
   document.getElementById('root')
 );
